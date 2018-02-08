5dfbdebd7e78160ccd3536a758d684198ee9b732
Manuel Pichardo <mlpichardo1@gmail.com>
2018-02-08 23:07:33 +0000

    started lecture 7 express

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ var answer = question.ask('What is the meaning of life?');
 console.log(answer);
 
 goodbye();
+
+// LECTURE 7 EXPRESS
+
+var express = require('express');
+var app = express();
+
+app.listen(3000);
+console.log('Magic happens on port 3000');
