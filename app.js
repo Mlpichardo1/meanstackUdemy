b48fc38278b7ca3928c58b29bd1d8e5c4d4dcf96
Manuel Pichardo <mlpichardo1@gmail.com>
2018-02-09 00:24:07 +0000

    starting middleware

--- app.js
+++ app.js
@@ -12,11 +12,31 @@ console.log(answer);
 goodbye();
 
 // LECTURE 7 EXPRESS
-
 var express = require('express');
 var app = express();
+var path = require("path");
 
 app.set('port', 3000);
 
-app.listen(3000);
-console.log('Magic happens on port 3000');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/json', function (res, req) {
+    console.log("GET the json");
+    res
+    .status(200)
+    .json( {'json data' : true} );
+});
+
+app.get('/file', function (res, req) {
+    console.log("GET the file");
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port
+
+console.log('Magic happens on port 3000 ' + port);
+});
