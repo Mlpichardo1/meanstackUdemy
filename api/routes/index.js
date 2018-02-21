de1188b4c0d29f959ffcb50bc70e5b75b4fe3944
Manuel Pichardo <mlpichardo1@gmail.com>
2018-02-21 01:57:12 +0000

    completed lecture 32

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,22 +6,19 @@ var ctrlReviews = require("../controllers/reviews.controllers.js");
 
 router
     .route('/hotels')
-    .get(ctrlHotels.hotelGetAll);
+    .get(ctrlHotels.hotelsGetAll)
+    .post(ctrlHotels.hotelsAddOne);
 
 router
     .route('/hotels:hotelId')
     .get(ctrlHotels.hotelGetOne);
 
-router
-    .route('/hotels/new')
-    .post(ctrlHotels.hotelAddOne);
-
 //Review routes
-
 router
     .route('/hotels/:hotelId/reviews')
-    .get(ctrlReviews.reviewsGetAll);
-
+    .get(ctrlReviews.reviewsGetAll)
+    .post(ctrlReviews.reviewsAddOne);
+    
 router
     .route('/hotels/:hotelId/reviews/:reviewId')
     .get(ctrlReviews.reviewsGetOne);
