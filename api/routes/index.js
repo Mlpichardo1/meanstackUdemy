606371656022e687e736b771c3267dcf50db4104
Manuel Pichardo <mlpichardo1@gmail.com>
2018-03-03 21:24:32 +0000

    login, auth, and register

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var ctrlHotels = require('../controllers/hotels.controllers.js');
 var ctrlReviews = require('../controllers/reviews.controllers.js');
-var ctrlUsers = require("../controllers/users.contollers.js");
+var ctrlUsers = require('../controllers/users.controllers.js');
 
 // Hotel routes
 router
@@ -21,20 +21,20 @@ router
 router
   .route('/hotels/:hotelId/reviews')
   .get(ctrlReviews.reviewsGetAll)
-  .post(ctrlReviews.reviewsAddOne);
+  .post(ctrlUsers.authenticate, ctrlReviews.reviewsAddOne);
 
 router
   .route('/hotels/:hotelId/reviews/:reviewId')
   .get(ctrlReviews.reviewsGetOne)
   .put(ctrlReviews.reviewsUpdateOne);
-  
-//Authentacation
+
+// Authentication
 router
   .route('/users/register')
   .post(ctrlUsers.register);
-  
+
   router
-  .route('/users/login')
-  .post(ctrlUsers.login);
+    .route('/users/login')
+    .post(ctrlUsers.login);
 
 module.exports = router;
